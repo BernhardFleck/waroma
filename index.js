19e7f55885fdc4793d5f9384527f095dee82f007
BerniFleck <dh211822@fhstp.ac.at>
2022-03-24 19:58:37 +0100

    implement first draft of a temporary table for connected devices and get its content on the index page

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
-const express = require('express');
-const { Socket } = require('socket.io');
-const app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-const PORT = 3000;
+/**
+ * Notes: 
+ * Always beautify your code before committing (On Windows: Shift + Alt + F)
+ * Never push unworking code
+ * Try to use readable names
+ */
 
-http.listen(PORT, () => console.log(`server listening on port ${PORT}!`));
+const express = require('express');
+const app = express();
+const PORT = 3000;
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let connectedDevices = []
+
 app.use(express.static('public'));
-app.get("/test", (request, response) => response.send("Hello World"))
+http.listen(PORT, () => console.log(`server listening on port ${PORT}!`));
+io.on('connection', async (socket) => {
+    let socketId = socket.id
+    console.log(`Client connected: ${socketId}`)
+
+    socket.on('GetAllDevices', function () {
+
+        console.log(`Print all devices onto the UI of client ${socketId}`)
+        connectedDevices.forEach(ipAddress =>
+            io.to(socket.id).emit("DeviceConnection", ipAddress))
+    });
+});
+
 app.get("/connect/:ip", (request, response) => {
+
     let ipAddress = request.params.ip
-    connectWristbandBy(ipAddress)
+    let databaseDoesNotContainIp = !connectedDevices.includes(ipAddress)
+
+    if (databaseDoesNotContainIp) {
+        updateDatabaseByInserting(ipAddress)
+        updateClientsAboutConnectionOf(ipAddress)
+        flashDisplayOfWristbandBy(ipAddress)
+    }
+
+    response.end()
 })
 
-function connectWristbandBy(ipAddress){
+function updateDatabaseByInserting(ipAddress) {
+    connectedDevices.push(ipAddress)
+}
+
+function updateClientsAboutConnectionOf(ipAddress) {
     io.emit("DeviceConnection", ipAddress)
     console.log(`Wristband with IP Address ${ipAddress} trys to connect`)
+}
+
+function flashDisplayOfWristbandBy(ipAddress) {
+    //TODO call the REST interface of the wristband
+    console.log("All Devices:")
+    connectedDevices.forEach(device => console.log(device))
 }
--- public/sockets.js
+++ public/sockets.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     var socket = io();
 
+    socket.emit("GetAllDevices")
+    console.log("Requested all devices")
+
     socket.on('DeviceConnection', function (ipAddress) {
         let newListNode = document.createElement('li');
         newListNode.textContent = ipAddress;
